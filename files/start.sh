6f56a58861d8ac5ea6032e1ffd4479252c8e2042
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-04-29 23:24:11 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -9,9 +9,11 @@ if [ ! -f /conf/on-complete.sh ]; then
 	cp /conf-copy/on-complete.sh /conf/on-complete.sh
 fi
 
-chmod +x /conf/on-complete.sh
+chmod +x /conf-copy/on-complete.sh
 touch /conf/aria2.session
+chown -R abc:www-data /conf
+chown -R abc:www-data /data
 
-darkhttpd /aria2-webui/docs --port 80 &
-darkhttpd /data --port 8080 &
-aria2c --conf-path=/conf/aria2.conf
+su abc -c "darkhttpd /aria2-webui/docs --port 8081" &
+su abc -c "darkhttpd /data --port 8080" &
+su abc -c "aria2c --conf-path=/conf/aria2.conf"
